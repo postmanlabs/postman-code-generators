45e87270735392758ffa411d065726b97c8d8647
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-08 19:17:58 +0530

    Feature/new codegens (#5)
    * Added out/ in .gitignore, removed bitbucket urls from package.json
    
    * changes in curl/package.json to remove system test and bitbucket urls
    
    * Removed test-system script from js-fetch/package.json and adding package-lock.json
    
    * added codegen js-jquery

---
+++ codegens/js-jquery/lib/util/parseBody.js
@@ -0,0 +1,48 @@
+var _ = require('../lodash'),
+    sanitize = require('./sanitize').sanitize;
+
+    /**
+     * Used to parse the body of the postman SDK-request and return in the desired format
+     *
+     * @param  {Object} request - postman SDK-request object
+     * @param  {Boolean} requestBodyTrim - whether to trim request body fields
+     * @param  {String} indentation - used for indenting snippet's structure
+     * @returns {String} - request body
+     */
+module.exports = function (request, requestBodyTrim, indentation) {
+    // used to check whether body is present in the request and return accordingly
+    if (request.body) {
+        var requestBody = '',
+            bodyMap,
+            enabledBodyList;
+
+        switch (request.body.mode) {
+            case 'raw':
+                if (!_.isEmpty(request.body[request.body.mode])) {
+                    requestBody += `${indentation}"data": ` +
+                        `${sanitize(request.body[request.body.mode], request.body.mode, requestBodyTrim)},\n`;
+                }
+                return requestBody;
+            case 'urlencoded':
+                enabledBodyList = _.reject(request.body[request.body.mode], 'disabled');
+                if (!_.isEmpty(enabledBodyList)) {
+                    bodyMap = _.map(enabledBodyList, function (value) {
+                        return `${indentation.repeat(2)}"${sanitize(value.key, request.body.mode, requestBodyTrim)}":` +
+                            ` "${sanitize(value.value, request.body.mode, requestBodyTrim)}"`;
+                    });
+                    requestBody = `${indentation}"data": {\n${bodyMap.join(',\n')}\n${indentation}}\n`;
+                }
+                return requestBody;
+            case 'formdata':
+                requestBody = `${indentation}"processData": false,\n` +
+                        `${indentation}"mimeType": "multipart/form-data",\n` +
+                        `${indentation}"contentType": false,\n` +
+                        `${indentation}"data": form\n`;
+                return requestBody;
+            default:
+                return requestBody;
+
+        }
+    }
+    return '';
+};
