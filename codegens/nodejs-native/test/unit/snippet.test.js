d00cb2a33bc9c4eae0f17e7b5b719ec531181d11
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-09 11:11:07 +0530

    Added nodejs-native

---
+++ codegens/nodejs-native/test/unit/snippet.test.js
@@ -0,0 +1,141 @@
+var expect = require('chai').expect,
+    fs = require('fs'),
+    sdk = require('postman-collection'),
+    exec = require('shelljs').exec,
+    newman = require('newman'),
+    parallel = require('async').parallel,
+
+    convert = require('../../lib/index').convert,
+    mainCollection = require('./fixtures/testcollection/collection.json');
+
+/**
+ * compiles and runs codesnippet then compare it with newman output
+ * 
+ * @param {String} codeSnippet - code snippet that needed to run using java
+ * @param {Object} collection - collection which will be run using newman
+ * @param {Function} done - callback for async call from mocha
+ */
+function runSnippet (codeSnippet, collection, done) {
+    fs.writeFile('run.js', codeSnippet, function (err) {
+        if (err) {
+            expect.fail(null, null, err);
+            return done();
+        }
+
+        var run = 'node run.js';
+
+        //  step by step process for compile, run code snippet, then comparing its output with newman
+        parallel([
+            function (callback) {
+                exec(run, function (err, stdout, stderr) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    if (stderr) {
+                        return callback(stderr);
+                    }
+
+                    try {
+                        stdout = JSON.parse(stdout);
+                    }
+                    catch (e) {
+                        console.error(e);
+                    }
+                    return callback(null, stdout);
+                });
+            },
+            function (callback) {
+                newman.run({
+                    collection: collection
+                }).on('request', function (err, summary) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    var stdout = summary.response.stream.toString();
+                    try {
+                        stdout = JSON.parse(stdout);
+                    }
+                    catch (e) {
+                        console.error(e);
+                    }
+                    return callback(null, stdout);
+                });
+            }
+        ], function (err, result) {
+            if (err) {
+                console.error(err);
+                expect.fail(null, null, err);
+            }
+            else if (typeof result[1] !== 'object' || typeof result[0] !== 'object') {
+                expect(result[0].trim()).to.equal(result[1].trim());
+            }
+            else {
+                const propertiesTodelete = ['cookies', 'headersSize', 'startedDateTime'],
+                    headersTodelete = [
+                        'accept-encoding',
+                        'user-agent',
+                        'cf-ray',
+                        'x-request-id',
+                        'x-request-start',
+                        'kong-request-id',
+                        'connect-time',
+                        'x-forwarded-for',
+                        'content-type',
+                        'content-length',
+                        'accept',
+                        'total-route-time',
+                        'cookie',
+                        'kong-cloud-request-id'
+                    ];
+                if (result[0]) {
+                    propertiesTodelete.forEach(function (property) {
+                        delete result[0][property];
+                    });
+                    if (result[0].headers) {
+                        headersTodelete.forEach(function (property) {
+                            delete result[0].headers[property];
+                        });
+                    }
+                }
+                if (result[1]) {
+                    propertiesTodelete.forEach(function (property) {
+                        delete result[1][property];
+                    });
+                    if (result[1].headers) {
+                        headersTodelete.forEach(function (property) {
+                            delete result[1].headers[property];
+                        });
+                    }
+                }
+                expect(result[0]).deep.equal(result[1]);
+            }
+            return done();
+        });
+    });
+}
+
+describe('nodejs-request convert function', function () {
+    mainCollection.item.forEach(function (item) {
+        it(item.name, function (done) {
+            var request = new sdk.Request(item.request),
+                collection = {
+                    item: [
+                        {
+                            request: request.toJSON()
+                        }
+                    ]
+                };
+            convert(request, {indentCount: 2, indentType: 'space'}, function (error, snippet) {
+                if (error) {
+                    expect.fail(null, null, error);
+                    return;
+                }
+                //  disabling eslint for test file
+                snippet = '/* eslint-disable */\n' + snippet;
+
+                runSnippet(snippet, collection, done);
+            });
+        });
+    });
+});
