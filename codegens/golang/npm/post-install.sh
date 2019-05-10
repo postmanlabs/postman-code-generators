ea0013e9db263c60a7df8a4928ed8cc74fa7f2ba
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-10 16:48:26 +0530

    Fixed post-install script for golang

--- codegens/golang/npm/post-install.sh
+++ codegens/golang/npm/post-install.sh
@@ -3,7 +3,7 @@ set -e;
 if [ $CI ]; 
   then
     echo "Installing additional dependencies"
-    sudo add-apt-repository ppa:jonathonf/golang
+    sudo add-apt-repository -y ppa:jonathonf/golang
     sudo apt-get update
     sudo apt-get install -y --force-yes golang-go
 fi
