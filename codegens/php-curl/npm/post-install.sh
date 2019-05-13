b8174fafa62a8afe1376793f98c0c8c7f9b08e9e
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-13 15:54:02 +0530

    adding sudo to apt-get install step in php-curl

--- codegens/php-curl/npm/post-install.sh
+++ codegens/php-curl/npm/post-install.sh
@@ -4,5 +4,6 @@ if [ $CI ];
   then
     echo "Installing additional dependencies"
     sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-    sudo apt-get update && apt-get install -y --force-yes php5.6-cli php-curl
+    sudo apt-get update
+    sudo apt-get install -y --force-yes php5.6-cli php-curl
 fi
