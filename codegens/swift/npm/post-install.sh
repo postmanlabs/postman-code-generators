320c602bdbaa0007ba2843c6d8a6177d7edf3386
Vishal Shingala <vishalkumar.shingala@getpostman.com>
2019-05-16 17:58:58 +0530

    changed name to swift

--- codegens/swift/npm/post-install.sh
+++ codegens/swift/npm/post-install.sh
@@ -9,8 +9,8 @@ if [ $CI ];
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
     sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev -y
-    wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
-    tar xzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
-    echo "export PATH=swift-4.2.1-RELEASE-ubuntu14.04/usr/bin:$PATH" >> ~/.bashrc
-    source  ~/.bashrc
+    sudo wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+    sudo tar xzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+    sudo echo "export PATH=swift-4.2.1-RELEASE-ubuntu14.04/usr/bin:$PATH" >> ~/.bashrc
+    sudo source  ~/.bashrc
 fi
