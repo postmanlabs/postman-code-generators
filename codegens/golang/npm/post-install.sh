34093acfa44ff4b2878c59ab22addb091f884e8a
Vishal Shingala <vishalkumar.shingala@getpostman.com>
2019-05-17 17:54:21 +0530

    Using git update-index --chmod for -x permission

--- codegens/golang/npm/post-install.sh
+++ codegens/golang/npm/post-install.sh
@@ -6,6 +6,4 @@ if [ $CI ];
     sudo add-apt-repository -y ppa:jonathonf/golang
     sudo apt-get update
     sudo apt-get install -y --force-yes golang-go
-    ls -la npm
-    ls -la ../swift/npm
 fi
