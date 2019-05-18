5f379e85e4e79cf0f81110a1f0254fcdbd005b96
Vishal Shingala <vishalkumar.shingala@getpostman.com>
2019-05-18 21:59:24 +0530

    using swiftenv git tool to install swift

--- codegens/swift/npm/post-install.sh
+++ codegens/swift/npm/post-install.sh
@@ -15,6 +15,7 @@ if [ $CI ];
     sudo echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
     sudo echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
     source ~/.bashrc
+    cat ~/.bashrc
     swiftenv install https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
     swift --version
 fi
