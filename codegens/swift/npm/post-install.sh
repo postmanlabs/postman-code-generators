6f3b93e0cdcb701053b5f40bef12c217bc66ec09
Vishal Shingala <vishalkumar.shingala@getpostman.com>
2019-05-18 21:43:55 +0530

    using swiftenv git tool to install swift

--- codegens/swift/npm/post-install.sh
+++ codegens/swift/npm/post-install.sh
@@ -9,11 +9,12 @@ if [ $CI ];
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
     sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev -y
-    sudo wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
-    sudo tar xzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
-    ls
-    sudo echo "export PATH=swift-4.2.1-RELEASE-ubuntu14.04/usr/bin:$PATH" >> ~/.bashrc
-    sudo cat ~/.bashrc
-    sudo which swift 
-    source  ~/.bashrc
+    mkdir .swiftenv
+    git clone https://github.com/kylef/swiftenv ~/.swiftenv
+    sudo echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bashrc
+    sudo echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    sudo echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
+    source ~/.bashrc
+    swiftenv install https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+    swift --version
 fi
