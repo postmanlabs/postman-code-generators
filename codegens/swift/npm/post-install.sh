967f09fe6c7aec2b1135f9e617345f72a1ee63c8
Vishal Shingala <vishalkumar.shingala@getpostman.com>
2019-05-18 22:51:22 +0530

    updated path of swift to contain /home/vishal first

--- codegens/swift/npm/post-install.sh
+++ codegens/swift/npm/post-install.sh
@@ -9,13 +9,9 @@ if [ $CI ];
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
     sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev -y
-    mkdir .swiftenv
-    git clone https://github.com/kylef/swiftenv ~/.swiftenv
-    sudo echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bashrc
-    sudo echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    sudo echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
-    source ~/.bashrc
-    cat ~/.bashrc
-    swiftenv install https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+    sudo wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+    sudo tar xzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+    sudo echo "export PATH=$HOME/swift-4.2.1-RELEASE-ubuntu14.04/usr/bin:$PATH" >> ~/.bashrc
+    source  ~/.bashrc
     swift --version
 fi
