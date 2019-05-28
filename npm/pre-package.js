6f80db63037af9122e2301d024c45dff6ead7a82
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-28 19:30:57 +0530

    Updated common functions to incorporate pre-package script

---
+++ npm/pre-package.js
@@ -0,0 +1,36 @@
+var fs = require('fs'),
+  path = require('path'),
+  codegens,
+  codegenList = [],
+  isDirectory = (source) => {
+    return fs.lstatSync(source).isDirectory();
+  },
+  readFile = (source) => {
+    return fs.readFileSync(source);
+  };
+
+const PATH_TO_UTIL_FOLDER = path.resolve(__dirname, '../lib/assets');
+
+// Try to get all the codegens directories from the path provided.
+// Catch error if any and return
+try {
+  codegens = fs.readdirSync('./codegens').map((name) => {
+    return `./codegens/${name}`;
+  }).filter(isDirectory);
+}
+catch (e) {
+  fs.writeFileSync(PATH_TO_UTIL_FOLDER + '/languages.json', JSON.stringify(codegenList), 'utf8');
+}
+
+if (codegens.length === 0) {
+  fs.writeFileSync(PATH_TO_UTIL_FOLDER + '/languages.json', JSON.stringify(codegenList), 'utf8');
+}
+
+codegens.forEach((codegen) => {
+  const content = readFile(`${codegen}/package.json`).toString(),
+    json = JSON.parse(content);
+  json.com_postman_plugin.main = `.${codegen}/${json.main}`;
+  codegenList.push(json.com_postman_plugin);
+});
+
+fs.writeFileSync(PATH_TO_UTIL_FOLDER + '/languages.json', JSON.stringify(codegenList), 'utf8');
