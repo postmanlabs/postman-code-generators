6866c8a48a6760e2e9eea1484d4689009b4be42b
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-30 13:33:10 +0530

    Updated swift version for xenial and removed

--- codegens/python-requests/npm/post-install.sh
+++ codegens/python-requests/npm/post-install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-set -e;
-if [ $CI ]; 
-  then
-    echo "Installing additional dependencies"
-    sudo apt-get update
-    sudo apt-get -y install python-pip
-    pip install requests
-fi
+# #!/bin/bash
+# set -e;
+# if [ $CI ]; 
+#   then
+#     echo "Installing additional dependencies"
+#     sudo apt-get update
+#     sudo apt-get -y install python-pip
+#     pip install requests
+# fi
