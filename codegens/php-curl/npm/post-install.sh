dc714e239fee381cbf774ef5b65e1923b8f9283a
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-30 12:31:29 +0530

    Trying out generic setting for trusty env in travis

--- codegens/php-curl/npm/post-install.sh
+++ codegens/php-curl/npm/post-install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-set -e;
-if [ $CI ]; 
-  then
-    echo "Installing additional dependencies"
-    sudo apt-get update
-    sudo apt-get install -y php5 php5-cli php5-curl
-fi
+# #!/bin/bash
+# set -e;
+# if [ $CI ]; 
+#   then
+#     echo "Installing additional dependencies"
+#     sudo apt-get update
+#     sudo apt-get install -y php5 php5-cli php5-curl
+# fi
