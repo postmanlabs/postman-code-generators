a88986c8619da78c2daa03aab92276bf7001a729
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-31 12:53:29 +0530

    Removed - from post-install script and added sudo java-unirtest/npm/post-install.sh

--- codegens/java-okhttp/npm/post-install.sh
+++ codegens/java-okhttp/npm/post-install.sh
@@ -3,12 +3,12 @@ set -e;
 if [ $CI ]; 
   then
     echo "Installing additional dependencies"
-    - sudo add-apt-repository ppa:openjdk-r/ppa -y
-    - sudo apt-get update && apt-get install -y openjdk-8-jdk;
-    - sudo mkdir dependencies
-    - cd dependencies
-    - sudo wget http://central.maven.org/maven2/com/squareup/okhttp3/okhttp/3.9.1/okhttp-3.9.1.jar
-    - sudo wget http://central.maven.org/maven2/com/squareup/okio/okio/1.14.0/okio-1.14.0.jar
-    - sudo touch main.java
-    - cd ..
+    sudo add-apt-repository ppa:openjdk-r/ppa -y
+    sudo apt-get update && apt-get install -y openjdk-8-jdk;
+    sudo mkdir dependencies
+    cd dependencies
+    sudo wget http://central.maven.org/maven2/com/squareup/okhttp3/okhttp/3.9.1/okhttp-3.9.1.jar
+    sudo wget http://central.maven.org/maven2/com/squareup/okio/okio/1.14.0/okio-1.14.0.jar
+    sudo touch main.java
+    cd ..
 fi
