3b2b54298b3b9a7356350bd58221d554291e2a65
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-05-31 12:40:29 +0530

    Added postinstall script key in package.json

--- codegens/java-okhttp/npm/post-install.sh
+++ codegens/java-okhttp/npm/post-install.sh
@@ -3,8 +3,8 @@ set -e;
 if [ $CI ]; 
   then
     echo "Installing additional dependencies"
-    - sudo add-apt-repository ppa:openjdk-r/ppa
-    - sudo apt-get update && apt-get install -y --force-yes openjdk-8-jdk;
+    - sudo add-apt-repository ppa:openjdk-r/ppa -y
+    - sudo apt-get update && apt-get install -y openjdk-8-jdk;
     - sudo mkdir dependencies
     - cd dependencies
     - sudo wget http://central.maven.org/maven2/com/squareup/okhttp3/okhttp/3.9.1/okhttp-3.9.1.jar
