a2d2b3176de99fc0f5eaf6e403ec30cce00a903e
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-06-06 15:39:26 +0530

    Removed swift --version line from post-install of swift

--- codegens/swift/npm/post-install.sh
+++ codegens/swift/npm/post-install.sh
@@ -11,5 +11,4 @@ if [ $CI ];
     sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev -y
     sudo wget https://swift.org/builds/swift-4.2.4-release/ubuntu1604/swift-4.2.4-RELEASE/swift-4.2.4-RELEASE-ubuntu16.04.tar.gz
     sudo tar xzf swift-4.2.4-RELEASE-ubuntu16.04.tar.gz
-    swift --version
 fi
