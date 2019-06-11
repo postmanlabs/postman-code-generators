b33cdc874a4220598227dde45474399821088c7e
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-06-11 13:49:19 +0530

    Fixed swift dependency installation

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -18,10 +18,12 @@ sudo apt-get -y install python-pip
 pip install requests
 
 echo "Installing dependencies required for tests in codegens/swift"
-sudo apt-get install libicu-dev git -y
-sudo apt-get install clang-3.6 -y
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
-sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev -y
-sudo wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
-sudo tar xzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+pushd ./codegens/swift &>/dev/null;
+  sudo apt-get install libicu-dev git -y
+  sudo apt-get install clang-3.6 -y
+  sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
+  sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
+  sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev -y
+  sudo wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+  sudo tar xzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
+popd &>/dev/null;
