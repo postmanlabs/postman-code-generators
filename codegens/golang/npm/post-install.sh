313613b40f5774091804de3531e3dbee6f6dfd47
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-06-11 13:20:50 +0530

    Moving all dependency installation step in one script, will run this only on travis. Removed the $CI check as it was being detected even on other pipelines

--- codegens/golang/npm/post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e;
-if [ $CI ]; 
-  then
-    echo "Installing additional dependencies"
-    sudo add-apt-repository -y ppa:jonathonf/golang
-    sudo apt-get update
-    sudo apt-get install -y --force-yes golang-go
-fi
