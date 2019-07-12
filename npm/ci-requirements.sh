ac891bdb2c5b1c0296b73403bb92c037b027daed
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-07-12 15:38:54 +0530

    Updated swift version

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -6,6 +6,8 @@ pushd ./codegens/swift &>/dev/null;
   sudo apt-get update
   sudo apt-get install clang-3.6 libicu-dev libpython2.7 -y
   sudo apt-get install libcurl3 libpython2.7-dev -y
-  sudo wget https://swift.org/builds/swift-4.0.3-release/ubuntu1604/swift-4.0.3-RELEASE/swift-4.0.3-RELEASE-ubuntu16.04.tar.gz
-  sudo tar xzf swift-4.0.3-RELEASE-ubuntu16.04.tar.gz
+  sudo wget https://swift.org/builds/swift-5.0.1-release/ubuntu1604/swift-5.0.1-RELEASE/swift-5.0.1-RELEASE-ubuntu16.04.tar.gz
+  sudo tar xzf swift-5.0.1-RELEASE-ubuntu16.04.tar.gz
+  sudo chmod 777 swift-5.0.1-RELEASE-ubuntu16.04/usr/lib/swift/CoreFoundation/module.modulemap
+  sudo export PATH=/swift-5.0.1-RELEASE-ubuntu16.04/usr/bin:$PATH
 popd &>/dev/null;
