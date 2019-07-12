7ff945c9fc035e7ffd624b89b385758247c6977f
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-07-12 16:11:10 +0530

    updated file permission for module.map in swift

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -8,6 +8,6 @@ pushd ./codegens/swift &>/dev/null;
   sudo apt-get install libcurl3 libpython2.7-dev -y
   sudo wget https://swift.org/builds/swift-5.0.1-release/ubuntu1604/swift-5.0.1-RELEASE/swift-5.0.1-RELEASE-ubuntu16.04.tar.gz
   sudo tar xzf swift-5.0.1-RELEASE-ubuntu16.04.tar.gz
-  sudo chmod 777 swift-5.0.1-RELEASE-ubuntu16.04/usr/lib/swift/CoreFoundation/module.modulemap
+  sudo chmod 777 swift-5.0.1-RELEASE-ubuntu16.04/usr/lib/swift/CoreFoundation/module.map
   sudo export PATH=/swift-5.0.1-RELEASE-ubuntu16.04/usr/bin:$PATH
 popd &>/dev/null;
