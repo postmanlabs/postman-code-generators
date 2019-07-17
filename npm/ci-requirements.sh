126305efbe4d49152b3dedf9c600ff7066322d3f
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-07-17 11:57:10 +0530

    Updated ci-requirements for jdk installation

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -2,6 +2,9 @@
 set -ev; # stop on error
 echo "Installing dependencies required for tests in codegens/java-okhttp"
 pushd ./codegens/java-okhttp &>/dev/null;
+  sudo add-apt-repository ppa:openjdk-r/ppa
+  sudo apt-get update
+  sudo apt-get install -y openjdk-8-jdk
   sudo wget http://central.maven.org/maven2/com/squareup/okhttp3/okhttp/3.9.1/okhttp-3.9.1.jar
   sudo wget http://central.maven.org/maven2/com/squareup/okio/okio/1.14.0/okio-1.14.0.jar
 popd &>/dev/null;
