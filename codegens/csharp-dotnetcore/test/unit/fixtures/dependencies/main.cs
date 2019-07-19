09363be0a507acb0ed9f23a7962aba69c07d4a68
Isaac Ilami <isaac.ilami@tylertech.com>
2019-07-19 10:26:12 -0500

    Reverted to old syntax in header snippet
    Also messed with var compile and var run, but they still throw errors

--- codegens/csharp-dotnetcore/test/unit/fixtures/dependencies/main.cs
+++ codegens/csharp-dotnetcore/test/unit/fixtures/dependencies/main.cs
@@ -4,11 +4,14 @@ using System.Text;
 using System.Threading.Tasks;
 namespace HttpRequests {
 	class Program {
-		static async Task Main(string[] args) {
+		static void Main(string[] args) {
+			Request().Wait();
+		}
+		static async Task Request() {
 			HttpClientHandler clientHandler = new HttpClientHandler();
 			HttpClient client = new HttpClient(clientHandler);
 			client.Timeout = TimeSpan.FromMilliseconds(5000);
-			string response = await client.GetStringAsync("https://704c30e8-77fe-4dc4-93e2-9c9c68dfb4e1.mock.pstmn.io/copy");
+			HttpResponseMessage response = await client.PostAsync("https://postman-echo.com/post", new StringContent("{\n  \"json\": \"Test-Test\"\n}", Encoding.UTF8, "application/json"));
 			Console.WriteLine(response.ToString());
 		}
 	}
