2cfa5bb0283813a194498f111d2575420fbb4e6c
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-07-25 17:32:48 +0530

    unskipped java-unirest and added dependencies in ci-requirements

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -9,6 +9,19 @@ pushd ./codegens/java-okhttp &>/dev/null;
   sudo wget http://central.maven.org/maven2/com/squareup/okio/okio/1.14.0/okio-1.14.0.jar
 popd &>/dev/null;
 
+echo "Installing dependencies required for tests in codegens/java-unirest"
+pushd ./codegens/java-unirest &>/dev/null;
+  sudo wget http://central.maven.org/maven2/com/mashape/unirest/unirest-java/1.4.9/unirest-java-1.4.9.jar
+  sudo wget http://central.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.2/httpclient-4.5.2.jar
+  sudo wget http://central.maven.org/maven2/commons-codec/commons-codec/1.9/commons-codec-1.9.jar
+  sudo wget http://central.maven.org/maven2/commons-logging/commons-logging/1.2/commons-logging-1.2.jar
+  sudo wget http://central.maven.org/maven2/org/apache/httpcomponents/httpcore/4.4.4/httpcore-4.4.4.jar
+  sudo wget http://central.maven.org/maven2/org/apache/httpcomponents/httpasyncclient/4.1.1/httpasyncclient-4.1.1.jar
+  sudo wget http://central.maven.org/maven2/org/apache/httpcomponents/httpcore-nio/4.4.4/httpcore-nio-4.4.4.jar
+  sudo wget http://central.maven.org/maven2/org/json/json/20160212/json-20160212.jar
+  sudo wget http://central.maven.org/maven2/org/apache/httpcomponents/httpmime/4.3.6/httpmime-4.3.6.jar
+popd &>/dev/null;
+
 echo "Installing dependencies required for tests in codegens/swift"
 pushd ./codegens/swift &>/dev/null;
   sudo apt-get update
@@ -21,5 +34,6 @@ popd &>/dev/null;
 
 echo "Installing dependencies required for tests in codegens/csharp-restsharp"
 sudo apt-get install -y mono-complete
+
 echo "Installing dependencies required for tests in codegens/shell-httpie"
 sudo apt-get install httpie
