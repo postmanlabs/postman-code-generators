4c74db7e13e46bc3b5872326f602f3dfda211313
Shrey Shah <shreyshah97@gmail.com>
2019-08-22 12:44:12 +0530

    Fixed failing swift tests

--- codegens/swift/lib/swift.js
+++ codegens/swift/lib/swift.js
@@ -75,13 +75,13 @@ function parseFormData (body, mode, trim, indent) {
 /**
  * Parses file body from the Request
  *
- * @param {String} indent - indentation string
  * @returns {String} request body in the desired format
  */
-function parseFile (indent) {
+function parseFile () {
   // var bodySnippet = 'let filename = "{Insert_File_Name}", postData = Data()\n';
   // bodySnippet += 'if let path = Bundle.main.path(forResource: filename, ofType: nil) {\n';
-  // bodySnippet += `${indent}do {\n${indent.repeat(2)}postData = try NSData(contentsOfFile:path, options:[]) as Data\n`;
+  // bodySnippet += `${indent}do {\n${indent.repeat(2)}postData =
+  // try NSData(contentsOfFile:path, options:[]) as Data\n`;
   // bodySnippet += `${indent}} catch {\n`;
   // bodySnippet += `${indent.repeat(2)}print("Failed to read from \\(String(describing: filename))")\n`;
   // bodySnippet += `${indent}}\n} else {\n`;
