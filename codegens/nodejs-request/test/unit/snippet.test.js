f160ef16d95743e110d26989107343b76a6ae2a9
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-08-26 15:31:42 +0530

    Common newman tests for codegen/nodejs-request

--- codegens/nodejs-request/test/unit/snippet.test.js
+++ codegens/nodejs-request/test/unit/snippet.test.js
@@ -1,148 +1,49 @@
 var expect = require('chai').expect,
-  fs = require('fs'),
   sdk = require('postman-collection'),
-  exec = require('shelljs').exec,
-  newman = require('newman'),
-  parallel = require('async').parallel,
-
+  async = require('async'),
+  newmanTestUtil = require('../../../../test/codegen/newman/newmanTestUtil'),
   sanitize = require('../../lib/util').sanitize,
   parseBody = require('../../lib/parseRequest').parseBody,
   getOptions = require('../../lib/index').getOptions,
   convert = require('../../lib/index').convert,
   mainCollection = require('./fixtures/testcollection/collection.json');
 
-/**
- * compiles and runs codesnippet then compare it with newman output
- *
- * @param {String} codeSnippet - code snippet that needed to run using java
- * @param {Object} collection - collection which will be run using newman
- * @param {Function} done - callback for async call from mocha
- */
-function runSnippet (codeSnippet, collection, done) {
-  fs.writeFile('run.js', codeSnippet, function (err) {
-    if (err) {
-      expect.fail(null, null, err);
-      return done();
-    }
-
-    var run = 'node run.js';
-
-    //  step by step process for compile, run code snippet, then comparing its output with newman
-    parallel([
-      function (callback) {
-        exec(run, function (err, stdout, stderr) {
-          if (err) {
-            return callback(err);
-          }
-          if (stderr) {
-            return callback(stderr);
-          }
-
-          try {
-            stdout = JSON.parse(stdout);
-          }
-          catch (e) {
-            console.error(e);
-          }
-          return callback(null, stdout);
-        });
-      },
-      function (callback) {
-        newman.run({
-          collection: collection
-        }).on('request', function (err, summary) {
-          if (err) {
-            return callback(err);
-          }
-
-          var stdout = summary.response.stream.toString();
-          try {
-            stdout = JSON.parse(stdout);
-          }
-          catch (e) {
-            console.error(e);
-          }
-          return callback(null, stdout);
-        });
-      }
-    ], function (err, result) {
-      if (err) {
-        console.error(err);
-        expect.fail(null, null, err);
-      }
-      else if (typeof result[1] !== 'object' || typeof result[0] !== 'object') {
-        expect(result[0].trim()).to.equal(result[1].trim());
-      }
-      else {
-        const propertiesTodelete = ['cookies', 'headersSize', 'startedDateTime', 'clientIPAddress'],
-          headersTodelete = [
-            'accept-encoding',
-            'user-agent',
-            'cf-ray',
-            'x-request-id',
-            'x-request-start',
-            'connect-time',
-            'x-forwarded-for',
-            'content-type',
-            'content-length',
-            'accept',
-            'total-route-time',
-            'cookie',
-            'kong-cloud-request-id',
-            'cache-control',
-            'postman-token',
-            'x-real-ip'
-          ];
-        if (result[0]) {
-          propertiesTodelete.forEach(function (property) {
-            delete result[0][property];
-          });
-          if (result[0].headers) {
-            headersTodelete.forEach(function (property) {
-              delete result[0].headers[property];
-            });
-          }
-        }
-        if (result[1]) {
-          propertiesTodelete.forEach(function (property) {
-            delete result[1][property];
-          });
-          if (result[1].headers) {
-            headersTodelete.forEach(function (property) {
-              delete result[1].headers[property];
-            });
-          }
-        }
-        expect(result[0]).deep.equal(result[1]);
-      }
-      return done();
-    });
-  });
-}
-
 describe('nodejs-request convert function', function () {
-  mainCollection.item.forEach(function (item) {
-    it(item.name, function (done) {
-      var request = new sdk.Request(item.request),
-        collection = {
-          item: [
-            {
-              request: request.toJSON()
-            }
-          ]
-        };
-      convert(request, {indentCount: 3, indentType: 'Space'}, function (error, snippet) {
+  var options = {indentCount: 2, indentType: 'Space'},
+    testConfig = {compileScript: null, runScript: 'node run.js', fileName: 'run.js'},
+    header = '/* eslint-disable */\n';
+
+  async.waterfall([
+    function (next) {
+      newmanTestUtil.generateSnippet(convert, options, function (error, snippets) {
         if (error) {
           expect.fail(null, null, error);
-          return;
+          return next(error);
         }
-        //  disabling eslint for test file
-        snippet = '/* eslint-disable */\n' + snippet;
 
-        runSnippet(snippet, collection, done);
+        return next(null, snippets);
       });
-    });
-  });
+    },
+    function (snippets, next) {
+      snippets.forEach((item, index) => {
+        it(item.name, function (done) {
+          newmanTestUtil.runSnippet(header + item.snippet, index, testConfig,
+            function (err, result) {
+              if (err) {
+                expect.fail(null, null, err);
+              }
+              if (typeof result[1] !== 'object' || typeof result[0] !== 'object') {
+                expect(result[0].toString().trim()).to.include(result[1].toString().trim());
+              }
+
+              expect(result[0]).deep.equal(result[1]);
+              return done(null);
+            });
+        });
+      });
+      return next(null);
+    }
+  ]);
 
   describe('Convert function', function () {
     var request,
