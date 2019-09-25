70b50eb268c00bd828d843ad7a54b58ff40f1d84
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2019-09-25 12:49:45 +0530

    Cleanup of temporary files and adding them to gitignore

--- codegens/python-http.client/test/unit/fixtures/codesnippet.py
+++
@@ -1,8 +0,0 @@
-import http.client
-conn = http.client.HTTPSConnection("9c76407d-5b8d-4b22-99fb-8c47a85d9848.mock.pstmn.io")
-payload = ''
-headers = {}
-conn.request("COPY", "", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
