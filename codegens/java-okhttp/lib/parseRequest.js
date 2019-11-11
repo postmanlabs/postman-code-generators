f8b044aa7fe625eca4857f1d4659d291eb4ede43
shreys7 <shreyshah97@gmail.com>
2019-11-12 00:13:32 +0530

    fixed java-okhttp tests

--- codegens/java-okhttp/lib/parseRequest.js
+++ codegens/java-okhttp/lib/parseRequest.js
@@ -130,6 +130,9 @@ function parseHeader (request, indentString) {
  * @returns {String}- content-type of request body
  */
 function parseContentType (request) {
+  if (request.body && request.body.mode === 'graphql') {
+    return 'application/json';
+  }
   return request.getHeaders({enabled: true, ignoreCase: true})['content-type'] || 'text/plain';
 }
 
