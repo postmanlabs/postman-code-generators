8c6231485da1f0741bc663b208100fe28b0ffc5f
shreys7 <shreyshah97@gmail.com>
2019-11-25 16:57:25 +0530

    Added unit test for single/double quotes in url fix in nodejs-unirest

--- codegens/nodejs-unirest/test/unit/snippet.test.js
+++ codegens/nodejs-unirest/test/unit/snippet.test.js
@@ -190,6 +190,42 @@ describe('nodejs unirest convert function', function () {
         expect(snippet).to.include('.attach(\'file\', \'/path/to/file\')');
       });
     });
+
+    it('should generate valid snippet for single/double quotes in url', function () {
+      var request = new sdk.Request({
+        'method': 'GET',
+        'header': [],
+        'url': {
+          'raw': 'https://postman-echo.com/get?query1=b\'b&query2=c"c',
+          'protocol': 'https',
+          'host': [
+            'postman-echo',
+            'com'
+          ],
+          'path': [
+            'get'
+          ],
+          'query': [
+            {
+              'key': 'query1',
+              'value': 'b\'b'
+            },
+            {
+              'key': 'query2',
+              'value': 'c"c'
+            }
+          ]
+        }
+      });
+      convert(request, {}, function (error, snippet) {
+        if (error) {
+          expect.fail(null, null, error);
+        }
+        expect(snippet).to.be.a('string');
+        // eslint-disable-next-line quotes
+        expect(snippet).to.include("'https://postman-echo.com/get?query1=b\\'b&query2=c\"c'");
+      });
+    });
   });
 
   describe('getOptions function', function () {
