9e13e7bb777f30297910991f4d8bff3d8caf83d7
shreys7 <shreyshah97@gmail.com>
2020-02-28 18:53:06 +0530

    Increase timeout to 2 minutes for java okhttp redirect test

--- codegens/java-okhttp/test/newman/newman.test.js
+++ codegens/java-okhttp/test/newman/newman.test.js
@@ -2,6 +2,7 @@ var runNewmanTest = require('../../../../test/codegen/newman/newmanTestUtil').ru
   convert = require('../../lib/index').convert;
 
 describe('convert for different request types', function () {
+  this.timeout(2 * 60000); // extending timeout to 2 minutes for java okhttp redirect tests
   var options = {indentCount: 3, indentType: 'Space', includeBoilerplate: true},
     testConfig = {
       compileScript: 'javac -cp *: main.java',
