f9270596cd61d9833cd5cba4c9f0cdcb92f1db20
someshkoli <kolisomesh27@gmail.com>
2020-03-28 04:01:43 +0530

    run and compile script changed

--- codegens/dart/test/newman/newman.test.js
+++ codegens/dart/test/newman/newman.test.js
@@ -12,10 +12,10 @@ describe('dart Converter', function () {
         // filename along with the appropriate version of the file. This file will be used to run the snippet.
         fileName: 'codesnippet.dart',
         // Run script required to run the generated code snippet
-        runScript: 'dart codesnippet.dart',
+        runScript: '/usr/lib/dart/bin/dart codesnippet.dart',
         // Compile script required to compile the code snippet
         // eslint-disable-next-line max-len
-        compileScript: 'pub get',
+        compileScript: '/usr/lib/dart/bin/pub get',
         // Array of name of collections for which newman tests has to be skipped.
         skipCollections: ['sameNameHeadersCollection', 'formdataCollection', 'formdataFileCollection']
       };
