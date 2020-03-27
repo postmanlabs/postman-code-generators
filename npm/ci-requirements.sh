66c0fc09eca71b25146641a857b00ab63b6e7c31
someshkoli <kolisomesh27@gmail.com>
2020-03-28 03:03:10 +0530

    Added ci-requirement scripts

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -46,3 +46,12 @@ sudo apt-get install -y mono-complete
 
 echo "Installing dependencies required for tests in codegens/shell-httpie"
 sudo apt-get install httpie
+
+echo "Installing dependencies required for tests in codegens/dart"
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install dart
+echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.profile
