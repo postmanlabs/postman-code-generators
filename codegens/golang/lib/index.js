b29981f82d5272073bfb9bbaac06829896657057
Somesh <kolisomesh27@gmail.com>
2020-06-02 01:57:27 +0530

    Adds snippet to check err before using res in GoNative  - adds err check after client.Do(req)  - adds println and return statement inside err condition body

--- codegens/golang/lib/index.js
+++ codegens/golang/lib/index.js
@@ -280,10 +280,13 @@ self = module.exports = {
       codeSnippet += `${indent}req.Header.Set("Content-Type", writer.FormDataContentType())\n`;
     }
     responseSnippet = `${indent}res, err := client.Do(req)\n`;
+    responseSnippet += `${indent}if err != nil {\n${indent.repeat(2)}fmt.Println(err)\n`;
+    responseSnippet += `${indent.repeat(2)}return\n${indent}}\n`;
     responseSnippet += `${indent}defer res.Body.Close()\n${indent}body, err := ioutil.ReadAll(res.Body)\n\n`;
     responseSnippet += `${indent}fmt.Println(string(body))\n}`;
 
     codeSnippet += responseSnippet;
+    console.log(codeSnippet);
     callback(null, codeSnippet);
   },
   getOptions: function () {
