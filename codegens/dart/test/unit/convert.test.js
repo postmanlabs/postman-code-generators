7d0eadc3d309655e476d099d763b17e0b10d7f37
Hussein <Hussein.Juybari+github@gmail.com>
2020-07-11 23:24:30 +0430

    Create test file for test all methods

---
+++ codegens/dart/test/unit/convert.test.js
@@ -0,0 +1,23 @@
+var convert = require('../../index').convert,
+  expect = require('chai').expect,
+  collection = require('./fixtures/collection.json'),
+  sdk = require('postman-collection'),
+  expectedSnippets = require('./fixtures/snippets.json');
+
+describe('Dart Converter', function () {
+  describe('convert for different request types', function () {
+    collection.item.forEach((item) => {
+      it(item.name, function (done) {
+        const request = new sdk.Request(item.request);
+        convert(request, {}, (err, snippet) => {
+          if (err) {
+            expect.fail(null, null, err);
+            return done();
+          }
+          expect(snippet).to.equal(expectedSnippets[item.name]);
+          return done();
+        });
+      });
+    });
+  });
+});
