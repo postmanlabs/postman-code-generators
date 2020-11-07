59e28aef03a8fa3213a9a8130cd4ebfd751f5190
Husseinhj <Hussein.Juybari@gmail.com>
2020-11-08 02:13:43 +0330

    Add new test file

--- codegens/kotlin-retrofit2/test/unit/convert.test.js
+++ codegens/kotlin-retrofit2/test/unit/convert.test.js
@@ -1,61 +1,131 @@
 var convert = require('../../index').convert,
   expect = require('chai').expect,
-  collection = require('./fixtures/collection.json'),
-  sdk = require('postman-collection'),
-  expectedSnippets = require('./fixtures/snippets.json');
+  sdk = require('postman-collection');
 
-describe('Kotlin-Retrofit2 Converter', function () {
-  describe('convert for different request types', function () {
-    collection.item.forEach((item) => {
-      it(item.name, function (done) {
-        const request = new sdk.Request(item.request);
-        convert(request, {
-          includeBoilerplate: true
-        }, (err, snippet) => {
-          if (err) {
-            expect.fail(null, null, err);
-            return done();
+// Disable check with expected snippets as we now have proper newman tests
+describe('Kotlin Converter', function () {
+  it('should add timeout if requestTimeout options is used', function () {
+    var request = new sdk.Request({
+      'method': 'POST',
+      'header': [
+        {
+          'key': 'Content-Type',
+          'value': 'application/json'
+        }
+      ],
+      'body': {
+        'mode': 'raw',
+        'raw': '{\n  "json": "Test-Test"\n}'
+      },
+      'url': {
+        'raw': 'https://postman-echo.com/post',
+        'protocol': 'https',
+        'host': [
+          'postman-echo',
+          'com'
+        ],
+        'path': [
+          'post'
+        ]
+      }
+    });
+
+    convert(request, {requestTimeout: 5000}, function (err, snippet) {
+      if (err) {
+        expect.fail(err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.contain('.connectTimeout(5000, TimeUnit.MILLISECONDS)');
+    });
+  });
+
+  it('should use http.MultipartRequest for formdata requests', function () {
+    var request = new sdk.Request({
+      'method': 'POST',
+      'header': [],
+      'body': {
+        'mode': 'formdata',
+        'formdata': []
+      },
+      'url': {
+        'raw': 'https://postman-echo.com/post',
+        'protocol': 'https',
+        'host': [
+          'postman-echo',
+          'com'
+        ],
+        'path': [
+          'post'
+        ]
+      }
+    });
+    convert(request, {}, function (err, snippet) {
+      if (err) {
+        expect.fail(err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.contain('@Multipart');
+    });
+  });
+
+  it('should add code for followRedirects if given in the option', function () {
+    var request = new sdk.Request({
+      'method': 'GET',
+      'header': [],
+      'url': {
+        'raw': 'https://postman-echo.com/',
+        'protocol': 'https',
+        'host': [
+          'postman-echo',
+          'com'
+        ]
+      }
+    });
+    convert(request, { followRedirect: false }, function (err, snippet) {
+      if (err) {
+        expect.fail(err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.contain('.followRedirects(false)');
+    });
+  });
+
+  it('should add boilerplate if given in the option', function () {
+    var request = new sdk.Request({
+      'method': 'GET',
+      'header': [],
+      'url': {
+        'raw': 'https://postman-echo.com/',
+        'protocol': 'https',
+        'host': [
+          'postman-echo',
+          'com'
+        ]
+      }
+    });
+    convert(request, { includeBoilerplate: true }, function (err, snippet) {
+      if (err) {
+        expect.fail(err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.contain('import retrofit2.Call');
+      expect(snippet).to.contain('fun main() {');
+    });
+  });
+
+  it('should add correct indentation', function () {
+    var request = new sdk.Request({
+      'method': 'POST',
+      'header': [],
+      'body': {
+        'mode': 'formdata',
+        'formdata': [
+          {
+            'key': 'hello',
+            'value': 'world',
+            'type': 'text'
           }
-
-          expect(snippet).to.equal(expectedSnippets[item.name]);
-          return done();
-        });
-      });
-    });
-  });
-});
-
-describe('Options Tests', function () {
-  it('should indent snippet with type and count specified', function () {
-    var request = new sdk.Request(collection.item[0].request);
-    convert(request, {
-      indentType: 'Tab',
-      indentCount: 2,
-      includeBoilerplate: true
-    }, function (error, snippet) {
-      if (error) {
-        expect.fail(null, null, error);
-      }
-      expect(snippet).to.be.a('string');
-      var snippetArray = snippet.split('\n'),
-        i;
-      for (i = 0; i < snippetArray.length; i++) {
-        if (snippetArray[i].startsWith('cachePolicy:NSURLRequestUseProtocolCachePolicy')) {
-          expect(snippetArray[i + 1].charAt(0)).to.equal('\t');
-          expect(snippetArray[i + 1].charAt(1)).to.equal('\t');
-          expect(snippetArray[i + 1].charAt(2)).to.not.equal(' ');
-        }
-      }
-    });
-  });
-
-  it('should use all the default options', function () {
-    var request = new sdk.Request({
-      'method': 'POST',
-      'header': [],
-      'body': {
-        'mode': 'raw',
-        'raw': '  trim this body  '
+        ]
       },
       'url': {
         'raw': 'https://postman-echo.com/post',
@@ -69,97 +139,12 @@ describe('Options Tests', function () {
         ]
       }
     });
-    convert(request, {}, function (error, snippet) {
-      if (error) {
-        expect.fail(null, null, error);
+    convert(request, { includeBoilerplate: true, indentType: 'Tab' }, function (err, snippet) {
+      if (err) {
+        expect.fail(err);
       }
       expect(snippet).to.be.a('string');
-      var snippetArray = snippet.split('\n'),
-        i;
-      for (i = 0; i < snippetArray.length; i++) {
-        if (snippetArray[i].startsWith('cachePolicy:NSURLRequestUseProtocolCachePolicy')) {
-          expect(snippetArray[i + 1].charAt(0)).to.equal(' ');
-          expect(snippetArray[i + 1].charAt(1)).to.equal(' ');
-          expect(snippet).to.include('timeoutInterval:10');
-          expect(snippet).to.include('  trim this body  ');
-        }
-      }
-    });
-  });
-
-  it('should add code for requestTimeout option', function () {
-    var request = new sdk.Request(collection.item[0].request);
-    convert(request, {
-      requestTimeout: 5000
-    }, function (error, snippet) {
-      if (error) {
-        expect.fail(null, null, error);
-      }
-      expect(snippet).to.be.a('string');
-      expect(snippet).to.include('timeoutInterval:5');
-    });
-  });
-
-  it('should trim request body when trimRequestBody is set to true', function () {
-    var request = new sdk.Request({
-      'method': 'POST',
-      'header': [],
-      'body': {
-        'mode': 'raw',
-        'raw': '  trim this body  '
-      },
-      'url': {
-        'raw': 'https://postman-echo.com/post',
-        'protocol': 'https',
-        'host': [
-          'postman-echo',
-          'com'
-        ],
-        'path': [
-          'post'
-        ]
-      }
-    });
-    convert(request, {
-      trimRequestBody: true
-    }, function (error, snippet) {
-      if (error) {
-        expect.fail(null, null, error);
-      }
-      expect(snippet).to.be.a('string');
-      expect(snippet).to.not.include('  trim this body  ');
-      expect(snippet).to.include('trim this body');
-    });
-  });
-
-  it('should include boiler plate if includeBoilerplate is set to true', function () {
-    var request = new sdk.Request({
-      'method': 'POST',
-      'header': [],
-      'body': {
-        'mode': 'raw',
-        'raw': '  trim this body  '
-      },
-      'url': {
-        'raw': 'https://postman-echo.com/post',
-        'protocol': 'https',
-        'host': [
-          'postman-echo',
-          'com'
-        ],
-        'path': [
-          'post'
-        ]
-      }
-    });
-    convert(request, {
-      includeBoilerplate: true
-    }, function (error, snippet) {
-      if (error) {
-        expect.fail(null, null, error);
-      }
-      expect(snippet).to.be.a('string');
-      expect(snippet).to.include('int main(int argc, const char * argv[])');
+      expect(snippet).to.contain('val hello0 = RequestBody.create(MediaType.parse("text/plain"), "world")');
     });
   });
 });
