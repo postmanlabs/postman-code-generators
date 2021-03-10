c336a3b6fe693b8d198d04ef6105861cc8bed466
Ankit Saini <ankitsaininitjsr@gmail.com>
2021-03-10 11:37:32 +0530

    In Ruby codegen, use JSON.dump for application/json body

--- codegens/ruby/lib/util/parseBody.js
+++ codegens/ruby/lib/util/parseBody.js
@@ -6,9 +6,10 @@ var _ = require('../lodash'),
  *
  * @param  {Object} request - postman SDK-request object
  * @param  {Boolean} trimRequestBody - whether to trim request body fields
+ * @param  {string} contentType - the content type of request body
  * @returns {String} - request body
  */
-module.exports = function (request, trimRequestBody) {
+module.exports = function (request, trimRequestBody, contentType) {
   // used to check whether body is present in the request and return accordingly
   if (request.body) {
     var requestBody = '',
@@ -17,10 +18,25 @@ module.exports = function (request, trimRequestBody) {
 
     switch (request.body.mode) {
       case 'raw':
-        if (!_.isEmpty(request.body[request.body.mode])) {
-          requestBody += 'request.body = ' +
-                        `${sanitize(request.body[request.body.mode], request.body.mode, trimRequestBody)}\n`;
+        if (_.isEmpty(request.body[request.body.mode])) {
+          return '';
         }
+
+        // Match any application type whose underlying structure is json
+        // For example application/vnd.api+json
+        // All of them have +json as suffix
+        if (contentType && (contentType === 'application/json' || contentType.match(/\+json$/))) {
+          try {
+            let jsonBody = JSON.parse(request.body[request.body.mode]);
+            return `request.body = JSON.dump(${JSON.stringify(jsonBody, null, 4)})\n`;
+          }
+          catch (error) {
+            // Do nothing
+          }
+        }
+
+        requestBody += 'request.body = ' +
+          `${sanitize(request.body[request.body.mode], request.body.mode, trimRequestBody)}\n`;
         return requestBody;
       case 'graphql':
         // eslint-disable-next-line no-case-declarations
