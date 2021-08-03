51a312988da1519fe3b58d31bb73700d9f6cfb29
Ankit Saini <ankitsaininitjsr@gmail.com>
2021-08-03 13:37:56 +0530

    Remove dependency on postman-collection in http codegen

--- codegens/http/lib/util.js
+++ codegens/http/lib/util.js
@@ -356,9 +356,64 @@ function addFormParam (array, key, type, val, disabled, contentType) {
   }
 }
 
+/**
+ * @param {Object} url - The Postman URL object
+ * @returns {String}
+ *
+ * Remove protocol from url if present
+ */
+function getHostWithoutProtocol (url) {
+  return url.host.join('.')
+    .replace(/^https?:\/\//, '');
+}
+
+/**
+ * @param {Object} url - The Postman URL object
+ * @returns {String}
+ *
+ * Return host from the URL
+ */
+function getHost (url) {
+  return getHostWithoutProtocol(url).split('/')[0];
+}
+
+/**
+ * @param {Object} url - The Postman URL object
+ * @returns {String}
+ *
+ * Return the path from a URL
+ */
+function getPath (url) {
+  var segments = [];
+  // Sometimes url.host contains protocol as well as path
+  // Extract that here
+  if (getHost(url).length !== url.host.join('.').length) {
+    console.log(getHostWithoutProtocol(url).split('/').slice(1));
+    url.path = getHostWithoutProtocol(url).split('/').slice(1).concat(url.path);
+  }
+  if (url.path) {
+    segments = _.reduce(url.path, function (res, segment) {
+      var variable;
+
+      // check if the segment has path variable prefix followed by the variable name.
+      if (segment.length > 1 && segment[0] === ':') {
+        variable = url.variables.one(segment.slice(1)); // remove path variable prefix.
+      }
+
+      variable = variable && variable.valueOf && variable.valueOf();
+      res.push(typeof variable === 'string' ? variable : segment);
+      return res;
+    }, []);
+  }
+
+  return '/' + segments.join('/'); // add leading slash
+}
+
 module.exports = {
   getHeaders: getHeaders,
   getBody: getBody,
   sanitizeOptions: sanitizeOptions,
-  addFormParam: addFormParam
+  addFormParam: addFormParam,
+  getPath: getPath,
+  getHost: getHost
 };
