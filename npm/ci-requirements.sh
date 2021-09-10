04561efc1fbabd60941db4a2085577efaebc901c
Justin Baur <admin@justinbaur.com>
2021-09-10 00:23:53 -0400

    Switch RestSharp to using 3.1

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -21,7 +21,7 @@ pushd ./codegens/csharp-restsharp &>/dev/null;
   sudo dpkg -i packages-microsoft-prod.deb
   sudo apt-get install apt-transport-https
   sudo apt-get update
-  sudo apt-get install dotnet-sdk-2.2
+  sudo apt-get install dotnet-sdk-3.1
   dotnet new console -o testProject
   pushd ./testProject &>/dev/null;
   dotnet add package RestSharp
