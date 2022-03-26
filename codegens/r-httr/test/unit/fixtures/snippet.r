ebbcc155a9a1994321de8eb045d7c9c95d49cac2
Erik Mendoza <efernandomendoza@gmail.com>
2022-03-25 23:49:42 -0600

    removing snippet.r file from tests

--- codegens/r-httr/test/unit/fixtures/snippet.r
+++
@@ -1,7 +0,0 @@
-library(httr)
-headers = c(
-  'Content-Type' = 'application/x-www-form-urlencoded'
-)
-body = upload_file('<file contents here>')
-res <- POST("https://postman-echo.com/post", body = body, add_headers(headers))
-cat(content(res, 'text'))
