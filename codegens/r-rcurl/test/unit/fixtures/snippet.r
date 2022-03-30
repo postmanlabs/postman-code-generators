9e38faa5f28498f2b5e9666a64d92a2c41c7ab95
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-03-30 13:21:34 -0600

    git ignore
    git ignore

--- codegens/r-rcurl/test/unit/fixtures/snippet.r
+++
@@ -1,7 +0,0 @@
-library(RCurl)
-headers = c(
-  "Content-Type" = "application/x-www-form-urlencoded"
-)
-params = "<file contents here>";
-res <- postForm("https://postman-echo.com/post", .opts=list(postfields = params, httpheader = headers, timeout.ms = 5000), style = "post")
-print(res)
