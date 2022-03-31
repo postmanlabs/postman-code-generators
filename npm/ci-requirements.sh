806c5f3343a10cffd1395ad08e8177229300bb10
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-03-31 12:03:40 -0600

    Update ci-requirements.sh

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -84,7 +84,7 @@ echo "Installing dependencies required for tests in codegens/php-guzzle"
   
 echo "Installing dependencies required for tests in codegens/r-rCurl Installing R"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/-cran40/'
+sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial-cran40/'
 sudo apt-get update
 sudo apt-get install r-base
 
