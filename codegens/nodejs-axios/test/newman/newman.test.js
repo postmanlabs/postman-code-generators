216e5044869883c69cdb81b4500edf9c30a6593e
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-11-01 15:33:49 -0600

    Update newman.test.js
    skip unsupportedmethods axios

--- codegens/nodejs-axios/test/newman/newman.test.js
+++ codegens/nodejs-axios/test/newman/newman.test.js
@@ -7,7 +7,8 @@ describe('Convert for different types of request', function () {
       compileScript: null,
       runScript: 'node run.js',
       fileName: 'run.js',
-      headerSnippet: '/* eslint-disable */\n'
+      headerSnippet: '/* eslint-disable */\n',
+      skipCollections: ['unsupportedMethods']
     };
 
   runNewmanTest(convert, options, testConfig);
@@ -20,7 +21,6 @@ describe('Convert for different types of request', function () {
         fileName: 'run.js',
         headerSnippet: '/* eslint-disable */\n',
         skipCollections: ['unsupportedMethods']
-
       };
 
     runNewmanTest(convert, options, testConfig);
