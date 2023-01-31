3be7d4462eda49f18d9a37d44211bebf6f009186
Ankit Saini <ankitsaininitjsr@gmail.com>
2023-01-31 14:05:23 +0530

    Enable testing for Swift

--- codegens/swift/test/newman/newman.test.js
+++ codegens/swift/test/newman/newman.test.js
@@ -1,7 +1,7 @@
 var runNewmanTest = require('../../../../test/codegen/newman/newmanTestUtil').runNewmanTest,
   convert = require('../../lib/index').convert;
 
-describe.skip('Convert for different types of request', function () {
+describe('Convert for different types of request', function () {
   var options = {
       indentType: 'Space',
       indentCount: 4
@@ -9,7 +9,7 @@ describe.skip('Convert for different types of request', function () {
     // if running locally on mac change the runScript to 'swift snippet.swift'
     testConfig = {
       fileName: 'snippet.swift',
-      runScript: 'swift-5.3-RELEASE-ubuntu16.04/usr/bin/swift snippet.swift',
+      runScript: 'swift-5.7.3-RELEASE-ubuntu20.04/usr/bin/swift snippet.swift',
       skipCollections: ['sameNameHeadersCollection', 'unsupportedMethods']
     };
   runNewmanTest(convert, options, testConfig);
