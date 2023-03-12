4d6571a67a57bd16eb626e4a2bb5cfb977c50e39
Nils Druyen <nils.druyen@fressnapf.com>
2023-03-12 10:39:50 +0100

    Update test dependencies

--- codegens/kotlin-okhttp/test/newman/newman.test.js
+++ codegens/kotlin-okhttp/test/newman/newman.test.js
@@ -5,9 +5,9 @@ describe('convert for different request types', function () {
   let options = {indentCount: 3, indentType: 'Space', includeBoilerplate: true},
     testConfig = {
       compileScript: null,
-      runScript: 'kotlinc -script main.kts -cp okhttp-4.9.0.jar:okio-2.10.0.jar',
+      runScript: 'kotlinc -script main.kts -cp okhttp-4.10.0.jar:okio-2.10.0.jar',
       fileName: 'main.kts',
-      skipCollections: ['redirectCollection']
+      skipCollections: ['redirectCollection', 'unsupportedMethods']
     };
   runNewmanTest(convert, options, testConfig);
 });
