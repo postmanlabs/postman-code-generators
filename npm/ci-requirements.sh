507c5000e5abba34a98540c2b0fcfce84e5e7f73
Aman Singh <121886615+aman-v-singh@users.noreply.github.com>
2023-06-12 15:53:55 +0530

    Update the dart version for dio library

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -82,8 +82,8 @@ sudo apt-get install httpie
 
 echo "Installing dependencies required for tests in codegens/dart"
 pushd ./codegens/dart-http &>/dev/null;
-  wget -q https://storage.googleapis.com/dart-archive/channels/stable/release/2.10.2/linux_packages/dart_2.10.2-1_amd64.deb
-  sudo dpkg -i dart_2.10.2-1_amd64.deb
+  wget -q https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/dart_3.0.4-1_amd64.deb
+  sudo dpkg -i dart_3.0.4-1_amd64.deb
   echo '''name: test
 dependencies:
   http: ^0.12.2
