688dc18998f7d5ba772d106f2458712fec1d472f
Aman Singh <121886615+aman-v-singh@users.noreply.github.com>
2023-06-14 23:55:28 +0530

    Update

--- npm/ci-requirements.sh
+++ npm/ci-requirements.sh
@@ -82,7 +82,7 @@ sudo apt-get install httpie
 
 echo "Installing dependencies required for tests in codegens/dart-http"
 pushd ./codegens/dart-http &>/dev/null;
-    wget -q https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/dart_3.0.4-1_amd64.deb
+  wget -q https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/dart_3.0.4-1_amd64.deb
   sudo dpkg -i dart_3.0.4-1_amd64.deb
   echo '''name: test
 version: 1.0.0
