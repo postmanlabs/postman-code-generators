e3c280637d0a94ab15cf38c80a4ef7c6a32a8e62
Parth Verma <parth.verma@postman.com>
2025-02-25 21:03:20 -0800

    fix: Failing builds for R and httpie

--- codegens/r-rcurl/test/ci-install.sh
+++ codegens/r-rcurl/test/ci-install.sh
@@ -5,5 +5,5 @@ echo "Installing dependencies required for tests in codegens/r-httr"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 sudo apt-get update
-sudo apt-get install r-base
+sudo apt-get install r-base libcurl4-gnutls-dev
 sudo R --vanilla -e 'install.packages("RCurl", version="1.98.1.6", repos="http://cran.us.r-project.org")'
