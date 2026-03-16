9d07f783421f8deb6496117687a8a4b5bd567b11
Dev Bachani <dev22603@gmail.com>
2026-03-16 19:46:12 +0200

    Gets rid of the extra import json

--- codegens/python-requests/lib/python-requests.js
+++ codegens/python-requests/lib/python-requests.js
@@ -113,7 +113,7 @@ self = module.exports = {
     }
     // For GraphQL requests, the payload is always JSON-encoded in the generated snippet,
     // so ensure the json module is imported even if the Content-Type header was not set.
-    if (request.body && request.body.mode === 'graphql') {
+    else if (request.body && request.body.mode === 'graphql') {
       snippet += 'import json\n';
     }
 
